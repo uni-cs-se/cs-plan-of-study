ed7590b27010f004a822a53c3c08826c3428344d
Kylie Duhachek <kylieduhachek@195-125.wireless.uni.edu>
2024-03-29 08:44:51 -0500

    Degree Requirements

--- degreerequirements.py
+++ degreerequirements.py
@@ -1,9 +1,12 @@
+import json
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 universities = ["Univeristy of Northern Iowa", "University of Iowa", "University of Iowa State"]
 
-@app.get("/")
-def read_root():
-    return universities
+course_lists = {'000': ['CS1', 'CS2', 'C3'], '001': ['CS1', 'CS2', 'CS3'], '002': ['CS1', 'CS2', 'CS3']  }
+@app.get("/courses/{university_id}")
+def read_root(university_id):
+    return json.dumps(course_lists[university_id])
